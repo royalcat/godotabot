61ba5fb04e17f97e60c967629d9b3e67eb4c9380
RoyalCat <k.adamovich20@gmail.com>
2018-02-11 00:30:18 +0500

    gggg

--- bot.js
+++ bot.js
@@ -96,6 +96,22 @@ client.on('message', (message) => {
 			message.channel.send("Pink dog toy");
 		}
     }
+	if(MSG == prefix +" i")
+    {
+		if(message.member.voiceChannel != null)
+		{
+			var voiceChannel = message.member.voiceChannel;
+			voiceChannel.join().then(connection =>{
+				const dispatcher = connection.playFile('./media/ilum.mp3');
+				dispatcher.setVolume(1);
+			})
+			setTimeout(leaveVoice, 5000, voiceChannel);
+		}
+		else
+		{
+			message.channel.send("Pink dog toy");
+		}
+    }
 	
     if(MSG == "Уди")
     {
