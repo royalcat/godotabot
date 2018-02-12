ee459f7f937cbaec8fd9fa0d2fc13ac6a6f3fd61
RoyalCat <k.adamovich20@gmail.com>
2018-02-12 15:31:26 +0300

    test

--- bot.js
+++ bot.js
@@ -66,6 +66,7 @@ client.on('message', (message) => {
 
     if(MSG.startsWith(prefix + " sound"))
     {
+		console.log(MSG.split[3]);
 		var mp3Duration = require('mp3-duration');
 		mp3Duration('file.mp3', function (err, duration) {
   			if (err) return console.log(err.message);
