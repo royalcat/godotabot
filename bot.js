5b269d9d4a9f042d6a3d932995b67e01c31d0145
RoyalCat <k.adamovich20@gmail.com>
2018-04-01 20:11:25 +0500

    blya x2

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ client.on('message', (message) =>
 
 	if(message.author.bot) return; // other bots ignore
 
-	if(MSG = "nein mode")
+	if(MSG == "nein mode")
 	{
 		neinMode = !neinMode;
 		message.channel.send("NEIN MODE ACTIVATED");
